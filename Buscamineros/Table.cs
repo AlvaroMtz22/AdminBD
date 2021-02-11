b0f43e894e23e4b6aa61f913b9e1d14500195275
Álvaro Martínez Gómez <alvamargo@gmail.com>
2021-02-11 13:28:23 +0100

    changes in class Table

--- Buscamineros/Table.cs
+++ Buscamineros/Table.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Buscamineros
 {
@@ -19,12 +20,12 @@ namespace Buscamineros
 
         public void DeleteTableColumn(TableColumn tableColumn)
         {
-            
+            m_list.Remove(tableColumn);
         }
 
         public void AddTableColumn(TableColumn tableColumn)
         {
-            
+            m_list.Add(tableColumn);
         }
 
         public string getName()
@@ -34,7 +35,30 @@ namespace Buscamineros
 
         public void AddRow(List<string> values)
         {
-     
+            if (values.Count == m_list.Count)
+            {
+                for (int i = 0; i < values.Count; i++)
+                {
+                    //string value = values.get(i);
+
+                }
+            }
+
+        }
+
+        public List<int> Where(string column, string name)
+        {
+            List<int> positions = null;
+            foreach(TableColumn x in m_list)
+            {
+                if(x.getName()==(column))
+                {
+                    //positions = (x.Where(name));
+                }
+            }
+
+            return positions; 
+
         }
 
     }
