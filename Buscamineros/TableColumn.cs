38061cc7e388e3acfda3ed8e8b468935613ec124
Unai Ruiz Martínez De Murguía <unairuiz.info@gmail.com>
2021-02-18 12:04:40 +0100

    new methods implemented

--- Buscamineros/TableColumn.cs
+++ Buscamineros/TableColumn.cs
@@ -20,13 +20,41 @@ namespace Buscamineros
 
         public void addValue(string value)
         {
-            
+            m_data.Add(value);
         }
+
 
         public string getName()
         {
             return m_name;
         }
+
+        public List<int> getPositions(string value)
+        {
+            List<int> positions = null;
+            for (int i = 0; i < m_data.Count; i++)
+            {
+                if (value == m_data.ElementAt(i))
+                {
+                    positions.Add(i);
+                }
+            };
+
+            return positions;
+        }
+
+        public List<string> getValues(List<int> positions)
+        {
+            List<string> list = null;
+            foreach (int p in positions)
+            {
+                string value = m_data.ElementAt(p);
+                list.Add(value);
+            }
+            return list;
+        }
+
+
         public List<string> getList()
         {
             return m_data;
