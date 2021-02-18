ae81ac2567a1bfb66c728e49f9f967cf6b70823c
Aitor Urabain Alvarez de Arcaya <aitoruaa@gmail.com>
2021-02-18 12:36:18 +0100

    select in another way

--- Buscamineros/Database.cs
+++ Buscamineros/Database.cs
@@ -51,10 +51,50 @@ namespace Buscamineros
             return m_tables;
         }
 
-        public List<string> select(string table, List<string> selects, String column, String name, string comparator)
+        public List<TableColumn> select(string table, List<string> selects, CompareWhere compared)
         {
-            List<string> values = null;
+            List<TableColumn> values = null;
             Table t = getTable(table);
+            List<TableColumn> TableColumns=t.getList();
+            List<string> val=null;
+            TableColumn column ;
+
+            foreach (TableColumn s in TableColumns) {
+                if (compared.getName().comparedTo(s.GetName())==0) 
+                {
+                    
+                    column = new TableColumn(s.GetName(), s.GetType() );
+                    if (compared.GetComparator().comparedTo("=") == 0)
+                    {
+                        foreach (string value in s.GetList())
+                        {
+                            if (value.CompareTo(compared.GetValue) == 0) {
+                                column.AddValue(value);
+                            }
+
+                        }
+                    }
+                    else if (compared.GetComparator().comparedTo("<") == 0)
+                    {
+                        foreach (string v in s.GetList())
+                        {
+
+
+                        }
+                    }
+                    else if (compared.getComparator().comparedTo("<=") == 0)
+                    {
+                    }
+                    else if (compared.getComparator().comparedTo(">") == 0)
+                    {
+                    }
+                    else if (compared.getComparator().comparedTo(">=") == 0)
+                    {
+                    }
+                    values.Add(column);
+                }
+                
+            }
             TableColumn tc = t.getColumn(selects.ElementAt(0));
             if (selects.Count == 1)
             {
