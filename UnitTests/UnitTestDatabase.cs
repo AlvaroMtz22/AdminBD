077551e31c0f8d42a7a18e047d5936a8fb2a86af
Aitor Urabain Alvarez de Arcaya <aitoruaa@gmail.com>
2021-02-25 13:08:50 +0100

    #4 unit test database done.

--- UnitTests/UnitTestDatabase.cs
+++ UnitTests/UnitTestDatabase.cs
@@ -23,15 +23,18 @@ namespace UnitTests
         [TestMethod]
         public void TestAddTable()
         {
+            System.Security.SecureString contraseña = new System.Security.SecureString();
+            database = new Database("aitor", "aitoru", contraseña);
             Table table = new Table("Nombres", data);
             tables.Add(table);
-            Assert.AreEqual(database.GetList().Count, 1);
             database.AddTable(table);
-            Assert.AreEqual(database.GetList(), tables);
+            Assert.AreEqual(database.GetList().Count, 1);
         }
         [TestMethod]
         public void TestDeleteTable()
         {
+            System.Security.SecureString contraseña = new System.Security.SecureString();
+            database = new Database("aitor", "aitoru", contraseña);
             //we add table table in order to have data to test if we are able to delete it.
             Table table = new Table("Nombres", data);
             database.AddTable(table);
@@ -45,9 +48,118 @@ namespace UnitTests
         public void TestGetName()
         {
             System.Security.SecureString contraseña = new System.Security.SecureString();
-            database = new Database("aitor", "aitoru", contraseña);
+            database = new Database("database", "aitoru", contraseña);
             String name = "database";
             Assert.AreEqual(name, database.GetName());
+        }
+        [TestMethod]
+        public void TestUpdateSet()
+        {
+            System.Security.SecureString contraseña = new System.Security.SecureString();
+            database = new Database("aitor", "aitoru", contraseña);
+            //Parameters that will be inserted in the function
+            List<string> setAttribute = new List<string>();
+            setAttribute.Add("Nombre");
+            setAttribute.Add("Apellido");
+            List<string> value = new List<string>();
+            value.Add("Aitor");
+            value.Add("Urabain");
+            string table = "Empleado";
+            CompareWhere compared = new CompareWhere("Nombre","Ronny","=");
+            //Creating the elements for changing with the update
+            Table tab = new Table("Empleado",new List<TableColumn>());
+            TableColumn tablecolumn1 = new TableColumn("Nombre","string");
+            tablecolumn1.AddValue("Alvaro");
+            tablecolumn1.AddValue("Ronny");
+            TableColumn tablecolumn2 = new TableColumn("Apellido", "string");
+            tablecolumn2.AddValue("Margo");
+            tablecolumn2.AddValue("Caiza");
+            tab.AddTableColumn(tablecolumn1);
+            tab.AddTableColumn(tablecolumn2);
+            database.AddTable(tab);
+            // executing the update method
+            database.updateSet(setAttribute, value, table, compared);
+            // looking if it has changed
+            Boolean welldone = false;
+            foreach (TableColumn tc in tab.GetList()) 
+            {
+                foreach (string st in tc.GetList()) 
+                {
+                    if (st == "Aitor") {
+                        welldone = true;
+                    }
+                }
+            }
+            Assert.AreEqual(true, welldone);
+        }
+        [TestMethod]
+        public void TestSelect()
+        {
+            System.Security.SecureString contraseña = new System.Security.SecureString();
+            database = new Database("aitor", "aitoru", contraseña);
+            //Creating elements for select method parameters
+            List<string> selects = new List<string>();
+            selects.Add("Nombre");
+            CompareWhere compared = new CompareWhere("Apellido", "Caiza", "=");
+            //Creating the elements for changing with the update
+            Table tab = new Table("Empleado", new List<TableColumn>());
+            TableColumn tablecolumn1 = new TableColumn("Nombre", "string");
+            tablecolumn1.AddValue("Alvaro");
+            tablecolumn1.AddValue("Ronny");
+            TableColumn tablecolumn2 = new TableColumn("Apellido", "string");
+            tablecolumn2.AddValue("Margo");
+            tablecolumn2.AddValue("Caiza");
+            tab.AddTableColumn(tablecolumn1);
+            tab.AddTableColumn(tablecolumn2);
+            database.AddTable(tab);
+            // executing the select method
+            Table result=database.select(tab.GetName(), selects,  compared);
+            // looking if it has changed
+            Boolean welldone = false;
+            foreach (TableColumn tc in result.GetList())
+            {
+                foreach (string st in tc.GetList())
+                {
+                    if (st == "Ronny")
+                    {
+                        welldone = true;
+                    }
+                }
+            }
+            Assert.AreEqual(true, welldone);
+        }
+        [TestMethod]
+        public void TestDelete()
+        {
+            System.Security.SecureString contraseña = new System.Security.SecureString();
+            database = new Database("aitor", "aitoru", contraseña);
+            CompareWhere compared = new CompareWhere("Nombre", "Ronny", "=");
+            //Creating the elements for changing with the update
+            Table tab = new Table("Empleado", new List<TableColumn>());
+            TableColumn tablecolumn1 = new TableColumn("Nombre", "string");
+            tablecolumn1.AddValue("Alvaro");
+            tablecolumn1.AddValue("Ronny");
+            TableColumn tablecolumn2 = new TableColumn("Apellido", "string");
+            tablecolumn2.AddValue("Margo");
+            tablecolumn2.AddValue("Caiza");
+            tab.AddTableColumn(tablecolumn1);
+            tab.AddTableColumn(tablecolumn2);
+            database.AddTable(tab);
+            // executing the update method
+            database.Delete(tab.GetName(),compared);
+            // looking if it has changed
+            Boolean welldone = false;
+            foreach (TableColumn tc in tab.GetList())
+            {
+                foreach (string st in tc.GetList())
+                {
+                    if (st == "Ronny")
+                    {
+                        welldone = true;
+                    }
+                }
+            }
+            Assert.AreNotEqual(true, welldone);
         }
     }
 }
