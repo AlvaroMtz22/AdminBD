09477f5350843370cb73b56389a1f38a7f11e15e
Unai Ruiz Martínez De Murguía <unairuiz.info@gmail.com>
2021-02-25 13:25:34 +0100

    #4 UnitTestTable done

--- UnitTests/UnitTestTable.cs
+++ UnitTests/UnitTestTable.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Buscamineros;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -9,13 +10,14 @@ namespace UnitTests
     public class UnitTestTable
     {
         Table table = new Table("Employees", new List<TableColumn>());
-        
+        List<TableColumn> list = new List<TableColumn>();
+
         [TestMethod]
         public void TableConstructorTest()
         {
             Table table2 = new Table("Employees", new List<TableColumn>());
-            Assert.AreEqual(table, table2);
-            
+            Assert.IsNotNull(table2);
+
         }
         [TestMethod]
         public void DeleteTableColumnTest()
@@ -30,27 +32,69 @@ namespace UnitTests
         [TestMethod]
         public void AddTableColumnTest()
         {
+            Table t = new Table("Users", new List<TableColumn>());
 
-            TableColumn tableColumn=new TableColumn("Aitor","string");
-            table.AddTableColumn(tableColumn);
-            Assert.AreEqual(1, table.GetList().Count);
+            Assert.AreEqual(0, t.GetList().Count);
+
+            TableColumn tableColumn = new TableColumn("Aitor", "string");
+            t.AddTableColumn(tableColumn);
+
+            Assert.AreEqual(1, t.GetList().Count);
+
         }
+
         [TestMethod]
-        public void getNameTest()
+        public void GetNameTest()
         {
             Assert.AreEqual(table.GetName(), "Employees");
         }
+
         [TestMethod]
         public void AddRowTest()
         {
+            List<TableColumn> list1 = new List<TableColumn>();
+            TableColumn name = new TableColumn("name", "string");
+            TableColumn surname = new TableColumn("surname", "string");
+            list1.Add(name);
+            list1.Add(surname);
+            Table table1 = new Table("Users", list1);
+
+            Assert.AreEqual(0,table1.GetList().ElementAt(0).GetList().Count);
+            Assert.AreEqual(0,table1.GetList().ElementAt(1).GetList().Count);
+
             List<string> values=new List<string>();
             values.Add("Aitor");
             values.Add("Urabain");
-            table.AddRow(values);
-            foreach (TableColumn x in table.GetList()) 
-            {
-                Assert.AreEqual(1, x.GetList().Count);
-            }
+            table1.AddRow(values);
+
+            Assert.AreEqual(1, table1.GetList().ElementAt(0).GetList().Count);
+            Assert.AreEqual(1, table1.GetList().ElementAt(1).GetList().Count);
+
+        }
+
+        [TestMethod]
+        public void GetListTest()
+        {
+            TableColumn tableColumn = new TableColumn("Aitor", "string");
+            table.AddTableColumn(tableColumn);
+
+            list.Add(tableColumn);
+
+            Assert.AreEqual(table.GetList().ElementAt(0), list.ElementAt(0));
+        }
+
+        [TestMethod]
+        public void GetColumnTest()
+        {
+            TableColumn tableColumn1 = new TableColumn("column1", "string");
+            table.AddTableColumn(tableColumn1);
+            TableColumn tableColumn2 = new TableColumn("column2", "string");
+            table.AddTableColumn(tableColumn2);
+            TableColumn tableColumn3 = new TableColumn("column3", "string");
+            table.AddTableColumn(tableColumn3);
+
+            Assert.AreEqual(table.GetColumn("column2"), tableColumn2);
+            Assert.AreNotEqual(table.GetColumn("column1"), tableColumn3);
 
         }
     }
