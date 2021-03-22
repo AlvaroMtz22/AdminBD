0cc43eb05c73ca3030d7189025b6a4cf4e769877
Álvaro Martínez Gómez <alvamargo@gmail.com>
2021-03-22 12:30:56 +0100

    #6 progress in parser

--- Buscamineros/MiniSQLParser/Parser.cs
+++ Buscamineros/MiniSQLParser/Parser.cs
@@ -14,8 +14,8 @@ namespace Buscamineros.MiniSQLParser
         {
             const string selectAllPattern = @"SELECT \* FROM ([a-zA-Z0-9]+)";
             const string selectColumnsPattern = @"SELECT ([a-zA-Z0-9,]+) FROM ([a-zA-Z0-9]+)";
-            const string deletePattern = @"DELETE FROM ([a-zA-Z0-9]+) WHERE ([a-zA-Z]+='[a-zA-Z0-9]+')";
-            const string updatePattern = @"UPDATE ([a-zA-Z0-9]+) SET ([a-zA-Z0-9]+='[a-zA-Z0-9]+'(,[a-zA-Z0-9]+='[a-zA-Z0-9]+')*) WHERE ([a-zA-Z0-9]+='[a-zA-Z0-9]+')";
+            const string deletePattern = @"DELETE FROM ([a-zA-Z0-9]+) WHERE ([a-zA-Z]+)([=><])'([a-zA-Z0-9]+)'";
+            const string updatePattern = @"UPDATE ([a-zA-Z0-9]+) SET ([a-zA-Z0-9]+='[a-zA-Z0-9]+'(,[a-zA-Z0-9]+='[a-zA-Z0-9]+')*) WHERE ([a-zA-Z]+)([=><])'([a-zA-Z0-9]+)'";
 
             Match match = Regex.Match(miniSqlSentence, selectAllPattern);
             if(match.Success)
@@ -29,6 +29,21 @@ namespace Buscamineros.MiniSQLParser
                 string[] columnNames = match.Groups[1].Value.Split(',');
                 SelectColumns selectColumns = new SelectColumns(match.Groups[2].Value,columnNames);
                 return selectColumns;
+            }
+            match = Regex.Match(miniSqlSentence, deletePattern);
+            if (match.Success)
+            {
+                CompareWhere cW = new CompareWhere(match.Groups[2].Value, match.Groups[4].Value, match.Groups[3].Value);
+                Delete delete = new Delete(match.Groups[1].Value, cW);
+                return delete;
+            }
+            match = Regex.Match(miniSqlSentence, updatePattern);
+            if (match.Success)
+            {
+                CompareWhere cW = new CompareWhere(match.Groups[2].Value, match.Groups[4].Value, match.Groups[3].Value);
+                string[] columnNames = match.Groups[1].Value.Split(',');
+                Update update = new Update(match.Groups[1].Value, columnNames);
+                return update;
             }
             return null;
         }
