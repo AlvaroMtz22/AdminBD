c141ef15ef508e6dabd80c83b2677b3d8550492a
Aitor Urabain Alvarez de Arcaya <aitoruaa@gmail.com>
2021-03-29 12:37:46 +0200

    #9 Main in Program made

--- ConsoleDatabase/Program.cs
+++ ConsoleDatabase/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using Buscamineros;
+using System;
+using System.IO;
 
 namespace ConsoleDatabase
 {
     class Program
     {
+        private static System.Security.SecureString contraseña; 
+        private static Database database;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (database == null) 
+            {
+                contraseña = new System.Security.SecureString();
+                database = new Database("MainDatabase", "AitorUrabain", contraseña); 
+            }
+            Console.WriteLine("Write the line you want to execute in the DB");
+            string linea = Console.ReadLine();
+            string queryResult = useDatabase(linea , database);
+            Console.WriteLine(queryResult);
+            
         }
     }
 }
