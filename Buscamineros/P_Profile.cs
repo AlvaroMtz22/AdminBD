a7e4ebb6b22c59ea31595b40708bd5465c7e987b
Ronny Caiza Guerrero <ronnycaiza@hotmail.com>
2021-04-15 13:26:20 +0200

    #10 Profile and User classed made

---
+++ Buscamineros/P_Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buscamineros
+{
+    class Profile
+    {
+        private string m_name;
+        private List<TableColumn> m_list = new List<TableColumn>();
+    }
+}
---
+++ Buscamineros/P_User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buscamineros
+{
+    class User 
+    
+    //name, password, profile (profile class)
+    {
+        
+        private string m_username;
+        private System.Security.SecureString m_password;
+        private Profile m_profile = new Profile();
+
+        public User (string username, System.Security.SecureString password, Profile profile)
+        {
+
+            m_username = username;
+            m_password = password;
+            m_profile = profile;
+        }
+        public string GetName()
+        {
+            return m_username;
+        }
+        
+
+        //Dictionary<string,List<Privilege>>
+        //table Profile = new Table();
+        //tableProfile["User Profiles"] = new List<Privilege>();
+        //tableProfile["User Profiles"].add(PrivilegeDelete);
+        //tableProfile["User Profiles"].add(PrivilegeInsert);
+        //tableProfile["User Profiles"].add(PrivilegeSelect);
+        //tableProfile["User Profiles"].add(PrivilegeUpdate);
+
+    }
+    
+
+  
+    
+}
