ff9155e40c506aebb72b32d0b3a4889a91c51c8a
Unai Ruiz Martínez De Murguía <unairuiz.info@gmail.com>
2021-04-19 13:27:00 +0200

    #4,#10 unit tests profile and unit tests user

---
+++ UnitTests/UnitTestProfile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Buscamineros;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestProfile
+    {
+        Table table = new Table("Employees", new List<TableColumn>());
+        List<PrivilegeType> m_privilege = new List<PrivilegeType>();
+        P_Profile profile = new P_Profile("Student", m_privilege);
+        PrivilegeType privilege = PrivilegeType.Select;
+        Dictionary<string, List<PrivilegeType>> hashtable = new Dictionary<string, List<PrivilegeType>>();
+
+        [TestMethod]
+        public void ProfileConstructorTest()
+        {
+            P_Profile p = new P_Profile("Programmer", m_privilege);
+            Assert.IsNotNull(p);
+        }
+
+        [TestMethod]
+        public void AddPrivilegesInTableTest()
+        {
+            m_privilege.Add(PrivilegeType.Select);
+            hashtable.Add(table.GetName(), m_privilege);
+
+            profile.addPrivilegesInTable(PrivilegeType.Select, table.GetName());
+
+        }
+
+        [TestMethod]
+        public void DeletePrivilegesInTableTest()
+        {
+
+        }
+
+    }
+}
